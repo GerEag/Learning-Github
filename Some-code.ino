011b8c3c6e243c3f05f38486b0be82c7fb4bffe5
GerEag <gge0866@louisiana.edu>
2015-10-27 23:36:45 -0500

    Revert "Revert "Fixing the code""
    This reverts commit ff7322d14ce703c1f98be27660e82df8743e035a.

--- Some-code.ino
+++ Some-code.ino
@@ -1,7 +1,9 @@
 #This is some code that I'm trying to do to figure out what is going on in Github
 import numpy as np
+from matplotlib import pyplot as plt
 
 t=np.linspace(0, 5, 100)
 y=np.sin(t)
-plt.plot=(t,y)
-show plt
+
+plt.plot(t,y)
+plt.show()
