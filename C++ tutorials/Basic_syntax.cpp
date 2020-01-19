659949d6245170e2c4a75dc0c91a4bb435b81d0b
gge0866 <gge0866@louisiana.edu>
2020-01-18 18:48:40 -0600

    Trying C++ tutorials

---
+++ C++ tutorials/Basic_syntax.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // #include is like imports in python
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+   cout << "Hello World\n"; // prints Hello World
+   int num = 5;
+   string object = " dogs!";
+   cout << "I have " + to_string(num) + object + "\n";
+   return 0;
+}
