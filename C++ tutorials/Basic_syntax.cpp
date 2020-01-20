69850ce70e5699d757e87226048e67f2dffa6db0
gge0866 <gge0866@louisiana.edu>
2020-01-20 17:11:19 -0600

    Added 'for loop' to main() function

--- C++ tutorials/Basic_syntax.cpp
+++ C++ tutorials/Basic_syntax.cpp
@@ -1,5 +1,12 @@
 #include <iostream> // #include is like imports in python
+#include <list>
+
 using namespace std;
+
+// extern is for variable sharing between files
+extern int num;
+
+string object;
 
 // main() is where program execution begins.
 int main() {
@@ -7,5 +14,13 @@ int main() {
    int num = 5;
    string object = " dogs!";
    cout << "I have " + to_string(num) + object + "\n";
+
+   // Declare and initialize a list
+   // list<int> range (0,10);
+   int i = 1; // iterator
+   for (i; i!=10;i++) {
+   	cout << "The value of i is : " << i << endl;
+   }
+
    return 0;
 }
