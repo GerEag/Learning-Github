efa656d22401b8ff08c7870e363ac6251eddc0a3
gge0866 <gge0866@louisiana.edu>
2022-05-10 21:55:13 -0500

    Modeling caffeine decay in the body

---
+++ caffeine_decay.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+###############################
+
+# caffeine_decay
+
+# simple model of the decay rate of caffeine from the body
+
+# Gerald Eaglin, ULL, 5/10/2022
+
+###############################
+
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
